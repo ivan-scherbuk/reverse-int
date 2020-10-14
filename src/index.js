7eec74515cd7bf3b80b6e0b0a13a7c16a19df30e
ivan-scherbuk <ramz2001ramz@mail.ru>
2020-10-14 13:18:48 +0300

    feat: add correct solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+	n = String(n);
+	if (n[0] === '-') n = n.slice(1);
+	if (n[-1] === '0') n = n.slice(0, -1);
+	return Number(n.split('').reverse().join(''));
 }
